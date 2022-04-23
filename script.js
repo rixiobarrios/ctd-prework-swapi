43ee5bab6c4f87fb416f1c28f2e3a05233b6f804
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-04-23 11:04:32 -0500

    Render to page on html

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let baseUrl = 'https://swapi.dev/api/people/1/';
 fetch(baseUrl)
     .then((response) => response.json())
     .then((json) => console.log(json));
+person.innerHtml = `<h1>${json.name}</h1>`;
 
 let button = document.querySelector('#button');
 
