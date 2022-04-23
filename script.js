1b48ab5baa0fc0fd6c9bdbee297dec2541185cd5
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-04-23 18:27:01 -0500

    Update Styling 99%

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ fetch(baseUrl)
     .then((json) => {
         console.log(json);
         person.innerHTML = `<h1>${json.name}</h1>`;
+        // person.innerHTML = `<h1>${json.homeworld}</h1>`;
+        // person.innerHTML = `<h1>${json.films[1]}</h1>`;
+        // person.innerHTML = `<h1>${json.mass}</h1>`;
+        // person.innerHTML = `<h1>${json.height}</h1>`;
+        // person.innerHTML = `<h1>${json.hair_color}</h1>`;
     });
 
-let button = document.querySelector('#button');
+// let button = document.querySelector('#button');
 
-function youClickedMe() {
-    console.log('You clicked me');
-}
+// function youClickedMe() {
+//     console.log('You clicked me');
+// }
 
-button.addEventListener('click', youClickedMe);
+// button.addEventListener('click', youClickedMe);
