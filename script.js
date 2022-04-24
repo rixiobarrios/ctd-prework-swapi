7f17efcbbbcaac9d4ff01e92f7aa352299f524d6
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-04-24 12:57:40 -0500

    Characters render at films

--- script.js
+++ script.js
@@ -8,18 +8,21 @@ fetch(charactersUrl)
     .then((res) => {
         // console.log(res.results);
         res.results.map((data) => {
-            // console.log(char.name);
+            // console.log(data);
             const node = document.createElement('div');
             const textNode = document.createTextNode(data.name);
             node.appendChild(textNode);
             node.addEventListener('click', function () {
-                characterDetails(data.name);
+                characterDetails(data.url);
             });
-            people.appendChild(node);
+            if (people) {
+                people.appendChild(node); //TypeError here
+            }
         });
     });
 
 function characterDetails(url) {
+    // console.log(url);
     fetch(url)
         .then((res) => res.json())
         .then((result) => {
@@ -27,30 +30,32 @@ function characterDetails(url) {
             const nameNode = document.createElement('h2');
             const nameText = document.createTextNode(result.name);
             nameNode.appendChild(nameText);
-            // const heightNode = document.createElement('div');
-            // const heightText = document.createTextNode(result.height);
-            // heightNode.appendChild(heightText);
-            // const massNode = document.createElement('div');
-            // const massText = document.createTextNode(result.mass);
-            // massNode.appendChild(massText);
-            // const hairNode = document.createElement('div');
-            // const hairText = document.createTextNode(result.hair_color);
-            // hairNode.appendChild(hairText);
-            // const skinNode = document.createElement('div');
-            // const skinText = document.createTextNode(result.skin_color);
-            // skinNode.appendChild(skinText);
-            // const eyeNode = document.createElement('div');
-            // const eyeText = document.createTextNode(result.eye_color);
-            // eyeNode.appendChild(eyeText);
+            const heightNode = document.createElement('div');
+            const heightText = document.createTextNode(
+                'height: ' + result.height
+            );
+            heightNode.appendChild(heightText);
+            const massNode = document.createElement('div');
+            const massText = document.createTextNode(result.mass);
+            massNode.appendChild(massText);
+            const hairNode = document.createElement('div');
+            const hairText = document.createTextNode(result.hair_color);
+            hairNode.appendChild(hairText);
+            const skinNode = document.createElement('div');
+            const skinText = document.createTextNode(result.skin_color);
+            skinNode.appendChild(skinText);
+            const eyeNode = document.createElement('div');
+            const eyeText = document.createTextNode(result.eye_color);
+            eyeNode.appendChild(eyeText);
             while (people.firstChild) {
                 people.removeChild(people.firstChild);
             }
             people.appendChild(nameNode);
-            // characters.appendChild(heightNode);
-            // characters.appendChild(massNode);
-            // characters.appendChild(hairNode);
-            // characters.appendChild(skinNode);
-            // characters.appendChild(eyeNode);
+            people.appendChild(heightNode);
+            people.appendChild(massNode);
+            people.appendChild(hairNode);
+            people.appendChild(skinNode);
+            people.appendChild(eyeNode);
         });
 }
 
@@ -66,7 +71,9 @@ fetch(filmsUrl)
             node.addEventListener('click', function () {
                 filmDetails(film.url);
             });
-            films.appendChild(node); //TypeError here
+            if (films) {
+                films.appendChild(node);
+            }
         });
     });
 
@@ -81,14 +88,30 @@ function filmDetails(url) {
             const crawlNode = document.createElement('p');
             const crawlText = document.createTextNode(result.opening_crawl);
             crawlNode.appendChild(crawlText);
-            const charNode = document.createElement('div');
-            const charText = document.createTextNode(result.characters); //showing as urls
-            charNode.appendChild(charText);
+            const charsNode = document.createElement('div');
+            result.characters.map((character) => {
+                // characterDetails(character);
+                fetch(character)
+                    .then((res) => res.json())
+                    .then((res) => {
+                        // console.log(res);
+                        const charNode = document.createElement('div');
+                        const charText = document.createTextNode(res.name);
+                        charNode.appendChild(charText);
+                        charsNode.appendChild(charNode);
+                    });
+            });
+            // const charsText = document.createTextNode(result.characters);
+            // charNode.appendChild(charsText);
             while (films.firstChild) {
                 films.removeChild(films.firstChild);
             }
             films.appendChild(titleNode);
             films.appendChild(crawlNode);
-            films.appendChild(charNode);
+            films.appendChild(charsNode);
         });
+}
+
+function callCharacter(url) {
+    fetch(url).then((res) => res.json());
 }
